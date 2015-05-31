95b86c6c9af193e6cf57a1c775805100d7738c3a
Anton Martsenyuk <ngc224@yandex-team.ru>
2015-05-31 19:15:07 +0300

    more changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 bundle_dir=$(pwd)/vimrc/bundle
 
 if [[ ! -d "$bundle_dir" ]]; then
@@ -7,13 +9,8 @@ if [[ ! -d "$bundle_dir" ]]; then
 fi
 
 git clone git://github.com/gmarik/Vundle.vim.git $bundle_dir/Vundle.vim
+
 vim +PluginInstall +qall
-
-sudo apt-get install python-flake8 ghc-mod
-
-#compile YCM
-cd $bundle_dir/YouCompleteMe && ./install.sh && cd -
-
 if [[ -x $HOME/.vim ]]; then
     mv $HOME/.vim $HOME/.vim.bak
 fi
@@ -24,3 +21,8 @@ fi
 
 ln -s $(pwd)/vimrc $HOME/.vim
 ln -s $(pwd)/vimrc/vimrc $HOME/.vimrc
+
+sudo apt-get install python-flake8 ghc-mod
+
+#compile YCM
+cd $bundle_dir/YouCompleteMe && ./install.sh && cd -
