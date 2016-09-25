80b313606aafbfa2898d57f5650a4897a25ecc74
Anton Martsenyuk <ngc224@yandex-team.ru>
2016-09-25 13:27:38 +0300

    Improve deploy

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-bundle_dir=$(pwd)/vimrc/bundle
+bundle_dir=$(pwd)/vim/bundle
 
 if [[ ! -d "$bundle_dir" ]]; then
     mkdir $bundle_dir
 fi
+
+for name in vim vimrc bashrc gitconfig bash_functions; do
+    if [[ -f $HOME/.$name || -d $HOME/.$name || -L $HOME/.$name ]]; then
+        mv $HOME/.$name $HOME/.$name\.bak
+    fi
+
+    ln -s $(pwd)/$name $HOME/.$name
+done
 
 if [[ ! -d "$bundle_dir/Vundle.vim" ]]; then
     git clone git://github.com/gmarik/Vundle.vim.git $bundle_dir/Vundle.vim
 fi
 
 vim +PluginInstall +qall
-
-for name in vim vimrc bashrc gitconfig bash_functions; do
-    if [[ -x $HOME/.$name ]]; then
-        mv $HOME/.$name $HOME/.$name\.bak
-    fi
-
-    ln -s $(pwd)/$name $HOME/.$name
-done
 
 # sudo apt-get install python-flake8 ghc-mod
 #
